fcafb5e0b121c92d8ac81bc9f7913af6326915e5
Bowen Li <bowenli2@andrew.cmu.edu>
2025-09-14 14:47:48 -0400

    wip

--- src/prpl_utils/spaces.py
+++ src/prpl_utils/spaces.py
@@ -23,6 +23,8 @@ class EnumSpace(gym.spaces.Space[_Element]):
     def sample(
         self, mask: Any | None = None, probability: Any | None = None
     ) -> _Element:
+        del mask  # Unused
+        del probability  # Unused
         return self.elements[self.np_random.choice(self.num_elements)]
 
     def contains(self, x: Any) -> bool:
